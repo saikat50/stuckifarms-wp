093bef34e7af2512f6de39b918c57973f94383c9
Daniel C Farr <danielcfarr@gmail.com>
2019-01-12 15:32:33 -0700

    #2

--- wp-content/themes/tcf-theme/includes/cta-newsletter.php
+++ wp-content/themes/tcf-theme/includes/cta-newsletter.php
@@ -6,21 +6,7 @@
         </p>
 
         <div class="newsletter-form py-4">
-            <form action="">
-                <div class="row">
-                    <div class="col-lg-6 mx-auto">
-                        <div class="row">
-                            <div class="col-lg-8">
-                                <input type="text" class="form-control form-control-lg" placeholder="email@email.com">
-                            </div>
-                            <div class="col-lg-4">
-                                <button class="btn btn-lg btn-theme btn-block">Sign Up Now</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-            </form>
+            <?php echo do_shortcode('[contact-form-7 id="126" title="Newsletter form"]'); ?>
         </div>
     </div>
 </section>
