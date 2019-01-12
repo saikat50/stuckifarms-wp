c5b403452e745f93c0f011ccb3651f8e421af0ce
Daniel C Farr <danielcfarr@gmail.com>
2019-01-12 15:34:36 -0700

    add title on home page

--- wp-content/themes/tcf-theme/header.php
+++ wp-content/themes/tcf-theme/header.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<title><?php wp_title(''); ?></title>
+		<title><?php wp_title(''); ?><?php if(is_front_page()) {bloginfo('name');}?></title>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php echo 'class="'.join(' ', get_body_class()).'"'.PHP_EOL; ?>>
