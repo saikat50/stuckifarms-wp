3d6783fb27545f539a607747a279e125c330a3f7
Daniel C Farr <danielcfarr@gmail.com>
2019-01-21 23:24:14 -0700

    buyer tools subpage

---
+++ wp-content/themes/tcf-theme/includes/subpage-buyer-tools.php
@@ -0,0 +1,12 @@
+<?php 
+
+page_title_block('Buyer Tools');
+
+load_include('buyer-tools-breadcrumbs', ['page' => $post]);
+
+?>
+<section class="section">
+    <div class="container">
+        <?php the_content(); ?>
+    </div>
+</section>
