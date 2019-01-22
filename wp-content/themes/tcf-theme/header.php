e02b6e6f445bd5d4716bc08cb41d6c8fc46b5caa
Daniel C Farr <danielcfarr@gmail.com>
2019-01-21 22:50:13 -0700

    fix video

--- wp-content/themes/tcf-theme/header.php
+++ wp-content/themes/tcf-theme/header.php
@@ -14,7 +14,7 @@
 		<div class="topbar bg-theme-alt text-white">
 			<div class="container">
 				<div class="d-flex">
-					<ul class="navbar-nav flex-row ml-md-auto">
+					<ul class="navbar-nav flex-row ml-md-auto hidden-lg-down">
 						<?php if($facebook = get_field('facebook', 'option')){ ?>
 						<li class="nav-item">
 							<a class="nav-link link-white p-2" href="<?php echo $facebook; ?>" target="_blank">
@@ -27,16 +27,17 @@
 			</div>
 		</div>
 		<div class="main-menu">
+			<div class="bg bg-theme-alt main-menu-bg"></div>
 			<div class="container">
 				<nav class="navbar navbar-expand-lg">
 					<a class="navbar-brand" href="<?php echo site_url(); ?>">
 						<img src="<?php echo the_field('logo', 'option'); ?>" alt="Planstin" class="img-fluid">
 					</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<button class="navbar-toggler align-self-end align-self-lg-middle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<i class="far fa-bars"></i>
 					</button>
 
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<div class="collapse navbar-collapse align-self-end align-self-xl-center" id="navbarSupportedContent">
 						<ul class="navbar-nav ml-auto">
 							<?php wp_nav_menu(array(
 								'container' => false,
