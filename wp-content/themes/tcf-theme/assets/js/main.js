c6af6adea8deba7ffdc6e0cf57b1f6f4d87c3c28
Daniel C Farr <danielcfarr@gmail.com>
2019-01-22 22:34:01 -0700

    customize dropdown with hover

--- wp-content/themes/tcf-theme/assets/js/main.js
+++ wp-content/themes/tcf-theme/assets/js/main.js
@@ -1,6 +1,38 @@
 $(function(){
   console.log('ready');
+  var dropdownLists = $('.main-menu .dropdown');
 
+  function toggleDropdown(_d, _m, shouldOpen){
+    _m.toggleClass('show', shouldOpen);
+    _d.toggleClass('show', shouldOpen);
+    $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+    if(shouldOpen){
+      dropdownLists.not(_d).filter('.show').each(function(){
+        toggleDropdown( $(this), $('.dropdown-menu', $(this)), false );
+      });
+    }
+  }
+  function onDropdownClick(e){
+    let _a = $(e.target),
+        href = _a.attr('href');
+      if(href && href.length > 1){
+        window.location.href = href;
+      }
+  }
+  function onDropdownHover (e) {
+    let _d = $(e.target).closest('.dropdown'),
+        _m = $('.dropdown-menu', _d);
+
+    setTimeout(function(){
+      let shouldOpen = _d.is(':hover');
+      toggleDropdown(_d, _m, shouldOpen);
+    }, e.type === 'mouseleave' ? 300 : 0);
+  }
+
+  dropdownLists
+    .on('mouseenter mouseleave', onDropdownHover)
+    .on('click', '.dropdown-toggle', onDropdownClick);
+    
   if( $('#property-slider').length ){
     $('#property-slider').lightSlider({
       gallery:true,
